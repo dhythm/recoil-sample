852bd046cc12d96f7ff81571ce21e4efc2305955
dhythm <yuta.okada.20@gmail.com>
2021-02-05 15:21:44 +0900

    feat: completed up to Basic Tutorial

--- src/App.js
+++ src/App.js
@@ -1,63 +1,200 @@
+import React, { useState } from "react";
 import {
   atom,
   RecoilRoot,
   selector,
   useRecoilState,
   useRecoilValue,
+  useSetRecoilState,
 } from "recoil";
 import "./App.css";
 
 function App() {
   return (
     <RecoilRoot>
-      <CharacterCounter />
+      <TodoList />
     </RecoilRoot>
   );
 }
 
-const textState = atom({
-  key: "textState",
-  default: "",
+const todoListState = atom({
+  key: "todoListState",
+  default: [],
 });
 
-function CharacterCounter() {
+const todoListFilterState = atom({
+  key: "todoListFilterState",
+  default: "Show All",
+});
+
+const filteredTodoListState = selector({
+  key: "filteredTodoListState",
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+
+    switch (filter) {
+      case "Show Completed":
+        return list.filter((item) => item.isComplete);
+      case "Show Uncompleted":
+        return list.filter((item) => !item.isComplete);
+      default:
+        return list;
+    }
+  },
+});
+
+const todoListStatsState = selector({
+  key: "todoListStatsState",
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompleteNum = todoList.filter((item) => item.isComplete).length;
+    const totalUncompletedNum = totalNum - totalCompleteNum;
+    const percentCompleted = totalNum === 0 ? 0 : totalCompleteNum / totalNum;
+
+    return {
+      totalNum,
+      totalCompleteNum,
+      totalUncompletedNum,
+      percentCompleted,
+    };
+  },
+});
+
+function TodoList() {
+  const todoList = useRecoilValue(filteredTodoListState);
+
   return (
-    <div>
-      <TextInput />
-      <CharacterCount />
-    </div>
+    <>
+      <TodoListStats />
+      <TodoListFilters />
+      <TodoItemCreator />
+
+      {todoList.map((todoItem) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
+    </>
   );
 }
 
-function TextInput() {
-  const [text, setText] = useRecoilState(textState);
+function TodoListStats() {
+  const {
+    totalNum,
+    totalCompleteNum,
+    totalUncompletedNum,
+    percentCompleted,
+  } = useRecoilValue(todoListStatsState);
 
-  const onChange = (event) => {
-    setText(event.target.value);
+  const formattedPercentCompleted = Math.round(percentCompleted);
+
+  return (
+    <ul>
+      <li>Total items: {totalNum}</li>
+      <li>Items completed: {totalCompleteNum}</li>
+      <li>Items not completed: {totalUncompletedNum}</li>
+      <li>Percent completed: {formattedPercentCompleted}</li>
+    </ul>
+  );
+}
+
+function TodoListFilters() {
+  const [filter, setFilter] = useRecoilState(todoListFilterState);
+
+  const updateFilter = ({ target: { value } }) => {
+    setFilter(value);
+  };
+
+  return (
+    <>
+      <select value={filter} onChange={updateFilter}>
+        <option value="Show All">All</option>
+        <option value="Show Completed">Completed</option>
+        <option value="Show Uncompleted">Uncompleted</option>
+      </select>
+    </>
+  );
+}
+
+function TodoItemCreator() {
+  const [inputValue, setInputValue] = useState("");
+  const setTodoList = useSetRecoilState(todoListState);
+
+  const addItem = () => {
+    setTodoList((oldTodoList) => [
+      ...oldTodoList,
+      {
+        id: getId(),
+        text: inputValue,
+        isComplete: false,
+      },
+    ]);
+    setInputValue("");
+  };
+
+  const onChange = ({ target: { value } }) => {
+    setInputValue(value);
   };
 
   return (
     <div>
-      <input type="text" value={text} onChange={onChange} />
-      <br />
-      Echo: {text}
+      <input type="text" value={inputValue} onChange={onChange} />
+      <button onClick={addItem}>Add</button>
     </div>
   );
 }
 
-const charCountState = selector({
-  key: "charCountState",
-  get: ({ get }) => {
-    const text = get(textState);
+let id = 0;
+function getId() {
+  return id++;
+}
 
-    return text.length;
-  },
-});
+function TodoItem({ item }) {
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+  const index = todoList.findIndex((listItem) => listItem === item);
 
-function CharacterCount() {
-  const count = useRecoilValue(charCountState);
+  const editItemText = ({ target: { value } }) => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      text: value,
+    });
 
-  return <>Character Count: {count}</>;
+    setTodoList(newList);
+  };
+
+  const toggleItemCompletion = () => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      isComplete: !item.isComplete,
+    });
+
+    setTodoList(newList);
+  };
+
+  const deleteItem = () => {
+    const newList = removeItemAtIndex(todoList, index);
+    setTodoList(newList);
+  };
+
+  return (
+    <div>
+      <input type="text" value={item.text} onChange={editItemText} />
+      <input
+        type="checkbox"
+        checked={item.isComplete}
+        onChange={toggleItemCompletion}
+      />
+      <button onClick={deleteItem}>X</button>
+    </div>
+  );
+}
+
+function replaceItemAtIndex(arr, index, newValue) {
+  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+}
+
+function removeItemAtIndex(arr, index) {
+  return [...arr.slice(0, index), ...arr.slice(index + 1)];
 }
 
 export default App;
