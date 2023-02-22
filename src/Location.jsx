7deabec54ca848e57bea30aecb5e4bd4c0fb9d07
dhythm <yuta.okada.20@gmail.com>
2023-02-22 23:18:22 +0900

    update

--- src/Location.jsx
+++ src/Location.jsx
@@ -12,24 +12,25 @@ const fooState = atom({
 const barState = atom({
   key: 'bar',
   default: 0,
-  // effects: [urlSyncEffect({ refine: number(), history: 'push' })]
-  effects: [urlSyncEffect({ refine: number() })]
+  effects: [urlSyncEffect({ refine: number(), history: 'push' })]
 })
 
 export const Location = () => {
   return (
     <RecoilRoot>
       <RecoilURLSyncJSON location={{ part: 'queryParams' }}>
-        <Child />
-        <Buttons />
-        <ul>
-          <li><Link to="/loc?foo=0">foo=0</Link></li>
-          <li><Link to="/loc?foo=1">foo=1</Link></li>
-        </ul>
-        <ul>
-          <li><Link to="/loc?bar=0">bar=0</Link></li>
-          <li><Link to="/loc?bar=1">bar=1</Link></li>
-        </ul>
+        <div style={{ padding: 8 }}>
+          <Child />
+          <Buttons />
+          <ul>
+            <li><Link to="/loc?foo=0">foo=0</Link></li>
+            <li><Link to="/loc?foo=1">foo=1</Link></li>
+            <li><Link to="/loc?bar=0">bar=0</Link></li>
+            <li><Link to="/loc?bar=1">bar=1</Link></li>
+            <li><button onClick={() => window.location.replace('/loc?foo=4')}>foo=4</button></li>
+            <li><button onClick={() => window.location.replace('/loc?bar=5')}>bar=5</button></li>
+          </ul>
+        </div>
       </RecoilURLSyncJSON>
     </RecoilRoot>
   )
@@ -44,8 +45,6 @@ const Buttons = () => {
       <ul>
         <li><button onClick={() => setFoo(2)}>foo=2</button></li>
         <li><button onClick={() => setFoo(3)}>foo=3</button></li>
-      </ul>
-      <ul>
         <li><button onClick={() => setBar(2)}>bar=2</button></li>
         <li><button onClick={() => setBar(3)}>bar=3</button></li>
       </ul>
